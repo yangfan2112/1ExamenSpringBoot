59af8c5628570a760167dbf870b13c8a200dbddc
yangfan2112 <145387043+yangfan2112@users.noreply.github.com>
2023-11-08 17:36:36 +0100

    Creado los DTO

---
+++ src/main/java/edu/cesur/fullstack/models/ArtistDTO.java
@@ -0,0 +1,7 @@
+package edu.cesur.fullstack.models;
+
+public class ArtistDTO {
+
+	private Integer id;
+	private String name;
+}
---
+++ src/main/java/edu/cesur/fullstack/models/ArtworkDTO.java
@@ -0,0 +1,10 @@
+package edu.cesur.fullstack.models;
+
+public class ArtworkDTO {
+
+	private Integer id;
+	private String title;
+	private String description;
+	private Integer artistld;
+	
+}
