4a78bc997c6ddaa561b18c086b13597ccb8f1cf3
yangfan2112 <145387043+yangfan2112@users.noreply.github.com>
2023-11-08 17:37:09 +0100

    Añadiendo lombok

--- src/main/java/edu/cesur/fullstack/models/ArtworkDTO.java
+++ src/main/java/edu/cesur/fullstack/models/ArtworkDTO.java
@@ -1,5 +1,12 @@
 package edu.cesur.fullstack.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ArtworkDTO {
 
 	private Integer id;
